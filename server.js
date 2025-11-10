2273dd19925a7e336cd9f94142d656ffdd9b8b5d
Hermamak <akherman@st.ug.edu.gh>
2025-11-10 09:49:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,8 @@ app.get("/bookings", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
